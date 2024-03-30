ed55126ead6e9cec66f0ccfc58a60f64630a69c6
Kadek Dendi <109197635+kadekdendi@users.noreply.github.com>
2024-03-31 03:23:57 +0700

    [IS-21] Update reducer master data

--- src/store/apps/master/type/index.js
+++ src/store/apps/master/type/index.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import toast from 'react-hot-toast'
 import axios from 'src/configs/axios'
 
-export const fetchMasterDataType = createAsyncThunk('appMasterType/fetchData', async params => {
+export const fetchMasterDataType = createAsyncThunk('appMasterType/fetchData', async (params, { rejectWithValue }) => {
   try {
     const response = await axios({
       method: 'GET',
@@ -11,41 +11,50 @@ export const fetchMasterDataType = createAsyncThunk('appMasterType/fetchData', a
     return response.data
   } catch (error) {
     toast.error(error.response.data.message)
+    return rejectWithValue([])
   }
 })
 
-export const fetchMasterDataTypeDetail = createAsyncThunk('appMasterType/fetchDataDetail', async id => {
-  try {
-    const response = await axios({
-      method: 'GET',
-      url: process.env.NEXT_PUBLIC_BASE_URL + '/master/type/' + id
-    })
-    return response.data
-  } catch (error) {
-    toast.error(error.response.data.message)
+export const fetchMasterDataTypeDetail = createAsyncThunk(
+  'appMasterType/fetchDataDetail',
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: process.env.NEXT_PUBLIC_BASE_URL + '/master/type/' + id
+      })
+      return response.data
+    } catch (error) {
+      toast.error(error.response.data.message)
+      return rejectWithValue({})
+    }
   }
-})
+)
 
 // ADD TYPE
-export const addMasterDataType = createAsyncThunk('appMasterType/addType', async (data, { getState, dispatch }) => {
-  try {
-    await axios({
-      method: 'post',
-      url: process.env.NEXT_PUBLIC_BASE_URL + '/master/type/create',
-      headers: {},
-      data
-    })
-    dispatch(fetchMasterDataType())
-    toast.success('Sukses Menambahkan Tipe')
-  } catch (error) {
-    toast.error(error.response.data.message)
+export const addMasterDataType = createAsyncThunk(
+  'appMasterType/addType',
+  async (data, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await axios({
+        method: 'post',
+        url: process.env.NEXT_PUBLIC_BASE_URL + '/master/type/create',
+        headers: {},
+        data
+      })
+      dispatch(fetchMasterDataType())
+      toast.success(response.data.message)
+    } catch (error) {
+      toast.error(error.response.data.message)
+      return rejectWithValue({})
+    }
   }
-})
+)
 
 // EDIT TYPE
 export const editMasterDataType = createAsyncThunk(
   'appMasterType/editType',
-  async ({ id, data }, { getState, dispatch }) => {
+  async ({ id, data }, { dispatch, rejectWithValue }) => {
     try {
       const response = await axios({
         method: 'PUT',
@@ -53,27 +62,31 @@ export const editMasterDataType = createAsyncThunk(
         data: data
       })
       dispatch(fetchMasterDataType())
-      toast.success('Sukses Merubah Tipe')
+      toast.success(response.data.message)
     } catch (error) {
       toast.error(error.response.data.message)
+      return rejectWithValue({})
     }
   }
 )
 
 // DELETE TYPE
-export const deleteMasterDataType = createAsyncThunk('appType/deleteType', async (id, { getState, dispatch }) => {
-  try {
-    const response = await axios({
-      method: 'DELETE',
-      url: '/master/type/' + id,
-    })
-    dispatch(fetchMasterDataType())
-    toast.success('Sukses Menghapus Tipe')
-    return response.data
-  } catch (error) {
-    toast.error(error.response.data.message)
+export const deleteMasterDataType = createAsyncThunk(
+  'appType/deleteType',
+  async (id, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await axios({
+        method: 'DELETE',
+        url: '/master/type/' + id
+      })
+      dispatch(fetchMasterDataType())
+      toast.success(response.data.message)
+    } catch (error) {
+      toast.error(error.response.data.message)
+      return rejectWithValue({})
+    }
   }
-})
+)
 
 export const appMasterTypeSlice = createSlice({
   name: 'appMasterType',
