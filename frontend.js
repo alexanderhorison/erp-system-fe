84c4a257daaa101f3c7961b43075892a3c868a66
root <root@tjahayaberkatabadi.com>
2024-04-06 16:01:16 +0000

    delete frontend.js

--- frontend.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3001; // Default to port 3000 if PORT is not set in the environment
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
