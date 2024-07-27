8c066d0c7e9c05f14ba11494664c7746adf42baa
dendijoil <dendijoil@gmail.com>
2024-07-28 00:45:13 +0700

    [ERP-127] Add font awsome and enhance ui card product (#26)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -115,6 +115,11 @@ const App = props => {
             <meta name='keywords' content='' />
             <meta name='viewport' content='initial-scale=1, width=device-width' />
             <meta name="robots" content="noindex, nofollow" />
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+            />
           </Head>
 
           <AuthProvider>
