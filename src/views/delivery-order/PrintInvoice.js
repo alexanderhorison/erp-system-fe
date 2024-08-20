78728fea973156d5eb1a0af3084b5f0d6f09b80b
farhadyanuar <farhad.yanuar@bithealth.co.id>
2024-08-20 11:06:25 +0700

    fixing code delivery order

--- src/views/delivery-order/PrintInvoice.js
+++ src/views/delivery-order/PrintInvoice.js
@@ -143,7 +143,7 @@ const PrintInvoice = ({ id }) => {
                         <Typography variant='h5'>Surat Jalan</Typography>
                       </MUITableCell>
                       <MUITableCell>
-                        <Typography variant='h5'>{`#${data.deliveryOrderId}`}</Typography>
+                        <Typography variant='h5'>{`#${data.code}`}</Typography>
                       </MUITableCell>
                     </TableRow>
                     <TableRow>
