c250680cb381f71e4be77626d80718747bd0b1c3
dendijoil <dendijoil@gmail.com>
2025-04-24 19:57:47 +0700

    Add build id for PWA

--- next.config.js
+++ next.config.js
@@ -1,25 +1,30 @@
-const path = require("path");
+const path = require('path')
 
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
   disableDevLogs: true,
   disable: !process.env.NEXT_PUBLIC_ENABLE_PWA,
-  cacheStartUrl: false, // Jangan cache halaman utama
-});
+  cacheStartUrl: false // Jangan cache halaman utama
+})
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   trailingSlash: true,
   reactStrictMode: false, // Set false if needed
   swcMinify: true,
-  webpack: (config) => {
+  // Add a function to generate a unique build ID for each deployment
+  generateBuildId: async () => {
+    // Return a timestamp-based ID to ensure uniqueness across deployments
+    return `build-v-1.0.0-${Date.now()}`
+  },
+  webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, "./node_modules/apexcharts-clevision"),
-    };
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
 
-    return config;
-  },
-});
+    return config
+  }
+})
