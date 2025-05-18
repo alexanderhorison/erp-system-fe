d73256472de74c2f32b1afdec162390d1fe22cac
dendijoil <dendijoil@gmail.com>
2025-05-19 02:20:29 +0700

    Fix modal employee

--- src/views/master/products/ModalAddMasterProduct.js
+++ src/views/master/products/ModalAddMasterProduct.js
@@ -261,7 +261,6 @@ export default function ModalAddMasterProduct({ open, setOpen, typeModal, id }) 
           </DialogContent>
           <DialogActions
             sx={{
-              // justifyContent: 'center',
               px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
               pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
             }}
