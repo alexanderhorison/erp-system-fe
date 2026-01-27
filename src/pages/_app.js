87a417e89b6273c3a27a16617f09822f3ec71c4e
dendijoil <dendijoil@gmail.com>
2026-01-27 09:19:47 +0700

    feat: Add web app manifest for development environment and update manifest link in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -109,6 +109,7 @@ const App = props => {
   const baseTitle = 'TBA Point of Sales'
   const pageTitle = env === 'development' ? `${baseTitle} - Dev` : baseTitle
   const faviconHref = env === 'development' ? '/favicon-dev.svg?v=2' : '/favicon.ico'
+  const manifestHref = env === 'development' ? '/manifest-dev.json' : '/manifest.json'
 
   return (
     <Provider store={store}>
@@ -119,7 +120,7 @@ const App = props => {
             <meta name='description' content={pageTitle} />
             <link rel="icon" href={faviconHref} />
             <link rel="shortcut icon" href={faviconHref} />
-            <link rel="manifest" href="/manifest.json" />
+            <link rel="manifest" href={manifestHref} />
             <meta name='keywords' content='' />
             <meta name='viewport' content='initial-scale=1, width=device-width' />
             <meta name="robots" content="noindex, nofollow" />
